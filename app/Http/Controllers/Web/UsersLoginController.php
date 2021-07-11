43c3ed2fdf6f63057a0814e2161ea5f712ab23bf
Saddam Hussain <sishack8@gmail.com>
2021-07-11 09:33:07 +0530

    pdate

--- app/Http/Controllers/Web/UsersLoginController.php
+++ app/Http/Controllers/Web/UsersLoginController.php
@@ -8,6 +8,7 @@ use Carbon\Carbon;
 use Gloudemans\Shoppingcart\Facades\Cart;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Session;
 
 class UsersLoginController extends Controller
 {
@@ -29,38 +30,41 @@ class UsersLoginController extends Controller
         ]);
 
         if ((Auth::guard('users')->attempt(['contact_no' => $request->username, 'password' => $request->password])) || (Auth::guard('users')->attempt(['email' => $request->username, 'password' => $request->password]))) {
-             // get cart from DB if it exists
-           
-           
-            // /** If Cart is Present **/
-           
-        // if (Cart::count() > 0 && !empty(Cart::content())) {
-            //     foreach ($cart as $product_id => $item) {
-
-            //             $product = explode(',', $item);
-            //             $quantity = $product[0];
-            //             $size_id1 = $product[1];
-            //             $color_id1 = $product[2];
-
-            //             $check_cart_product = DB::table('cart')
-            //                 ->where('user_id', Auth::guard('users')->user()->id)
-            //                 ->where('product_id', $product_id)
-            //                 ->count();
-
-            //             if ($check_cart_product < 1 ) {
-            //                 DB::table('cart')
-            //                     ->insert([
-            //                         'user_id' => Auth::guard('users')->user()->id,
-            //                         'product_id' =>  $product_id,
-            //                         'size_id' =>  $size_id1,
-            //                         'color_id' =>  $color_id1,
-            //                         'quantity' => (int)$quantity,
-            //                         'created_at' => Carbon::now()->setTimezone('Asia/Kolkata')->toDateTimeString(),
-            //                     ]);
-            //             }
-            //         }
-            // }
-
+        	//************Check Session Shopping Cart**********************
+            if (Session::has('cart') && !empty(Session::get('cart'))) {
+                $cart = Session::get('cart');
+                if (count($cart) > 0) {
+                    foreach ($cart as $product_id => $value) {
+                    $color = null;
+                    $size_id = null;
+                    if ( isset($value['color_id']) && !empty($value['color_id'])) {
+                        $color = $value['color_id'];
+                    }
+                    if ( isset($value['size_id']) && !empty($value['size_id'])) {
+                        $size_id = $value['size_id'];
+                    }
+                    $check_cart_product = DB::table('cart')
+                        ->where('product_id',$product_id)
+                        ->where('user_id',Auth::guard('users')->user()->id)
+                        ->count();
+                    if ($check_cart_product < 1 ) {
+                        DB::table('cart')
+                            ->insert([
+                                'user_id' => Auth::guard('users')->user()->id,
+                                'product_id' =>  $product_id,
+                                'size_id' => $size_id,
+                                'color_id' => $color,
+                                'quantity' => $value['quantity'],
+                                'shipping_charge' => $value['shipping_charge'],
+                                'tax_id' => $value['tax_id'],
+                                'created_at' => Carbon::now()->setTimezone('Asia/Kolkata')->toDateTimeString(),
+                            ]);
+                    }
+                    }
+                }
+                Session::forget('cart');
+                Session::save();
+             }
             return redirect()->intended('/');
         }
 
